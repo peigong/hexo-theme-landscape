28fd74684d4ca0a9cfb12c6d85d29a391fec493e
周培公 <peigong@foxmail.com>
2024-08-28 17:48:36 +0800

    helper

---
+++ scripts/landscape-category-prepare.js
@@ -0,0 +1,13 @@
+/**
+ * @fileoverview
+ */
+
+/**
+ * 
+ * @param { Model<CategorySchema> } categories 文章类别列表
+ */
+const landscapeCategoryPrepare = (categories) => {
+// weight
+}
+
+hexo.extend.helper.register('landscapeCategoryPrepare', landscapeCategoryPrepare)
---
+++ scripts/landscape-category-transform.js
@@ -0,0 +1,13 @@
+/**
+ * @fileoverview
+ */
+
+/**
+ * 
+ * @param { String } name 文章类别名称
+ */
+const landscapeCategoryTransform = (name) => {
+
+}
+
+hexo.extend.helper.register('landscapeCategoryTransform', landscapeCategoryTransform)

d07ef2192cf7eaf2f0252c6c3282cdb591d98e14
周培公 <peigong@foxmail.com>
2024-08-30 13:53:22 +0800

    cat

--- scripts/landscape-category-prepare.js
+++ scripts/landscape-category-prepare.js
@@ -6,8 +6,13 @@
  * 
  * @param { Model<CategorySchema> } categories 文章类别列表
  */
-const landscapeCategoryPrepare = (categories) => {
-// weight
+const landscapeCategoryPrepare = (categories, map) => {
+    map = map || {}
+    categories.forEach(it => {
+        const ext = map[it.name] || {}
+        Object.assign(it, ext)
+    })
+    return categories
 }
 
 hexo.extend.helper.register('landscapeCategoryPrepare', landscapeCategoryPrepare)
--- scripts/landscape-category-transform.js
+++ scripts/landscape-category-transform.js
@@ -2,12 +2,16 @@
  * @fileoverview
  */
 
-/**
- * 
- * @param { String } name 文章类别名称
- */
-const landscapeCategoryTransform = (name) => {
-
+const createLandscapeCategoryTransform = (map) => {
+    map = map || {}
+    /**
+     * 
+     * @param { String } name 文章类别名称
+     */
+    return (name) => {
+        const cat = map[name] || {}
+        return cat.alias || name
+    }
 }
 
-hexo.extend.helper.register('landscapeCategoryTransform', landscapeCategoryTransform)
+hexo.extend.helper.register('createLandscapeCategoryTransform', createLandscapeCategoryTransform)
